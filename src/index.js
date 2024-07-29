690956cfe02e944069f3fd48684ebf86cf815851
KMusyimi <kennmusyimi97@gmail.com>
2024-07-29 20:41:40 +0300

    first  commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import './style.css'
+
+let countries;
+window.addEventListener("load", ()=>{
+    getData();
+});
+
+async function getData(){
+   
+    const dataPromise = await fetch('https://restcountries.com/v3.1/all?fields=name,population,region,capital,flags');
+    const data = await dataPromise.json();
+    countries = data;
+    console.log('data :>> ', data);
+    console.log('countries.indexOf() :>> ', countries.indexOf(countries.name === "Kenya"));
+}
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: './src/index.js',
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devtool: "source-map",
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Development',
+            template: './src/index.html'
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: "defaults" }]
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ]
+
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'src'),
+        },
+        compress: true,
+        port: 5050,
+    },
+    optimization: {
+
+        runtimeChunk: 'single',
+
+    },
+    module: {
+
+    },
+};
