55275ed6d76b731b47ed32e009df4d84c0d43a6d
kenn mich <kennmusyimi97@gmail.com>
2024-07-30 22:52:37 +0300

    Update my dist file

--- dist/main.js
+++ dist/main.js
@@ -9,9 +9,16 @@
 
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ "./src/style.css");
+/* harmony import */ var _theme__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./theme */ "./src/theme.js");
+
 
 let countries;
 window.addEventListener("load", () => {
+  const _ = new _theme__WEBPACK_IMPORTED_MODULE_1__.ThemeSwitcher();
+  const dropdownBtn = document.querySelector(".dropdown_btn");
+  dropdownBtn.addEventListener("click", function () {
+    dropdownBtn.classList.toggle("expanded");
+  });
   getData();
 });
 async function getData() {
@@ -20,6 +27,69 @@ async function getData() {
   countries = data;
   console.log('data :>> ', data);
   console.log('countries.indexOf() :>> ', countries.indexOf(countries.name === "Kenya"));
+}
+
+/***/ }),
+
+/***/ "./src/theme.js":
+/*!**********************!*\
+  !*** ./src/theme.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   ThemeSwitcher: () => (/* binding */ ThemeSwitcher)
+/* harmony export */ });
+/**
+* Utility function to calculate the current theme setting.
+* Look for a local storage value.
+* Fall back to system setting.
+* Fall back to light mode.
+*/
+const darkHtml = `<img src="./assets/icons/moon_icon.png" alt="dark button icon"> dark mode`;
+const lightHtml = `<img src="./assets/icons/sun_icon.png" alt="light button icon"> light mode`;
+const themeBtn = document.querySelector("[data-theme-toggle]");
+const localStorageTheme = localStorage.getItem("theme");
+const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
+function calcSettingThemeStr(_ref) {
+  let {
+    localStorageTheme,
+    systemSettingDark
+  } = _ref;
+  if (localStorageTheme !== null) {
+    return localStorageTheme;
+  }
+  if (systemSettingDark.matches) {
+    return "dark";
+  }
+  return "light";
+}
+let currentThemeSetting = calcSettingThemeStr({
+  localStorageTheme: localStorageTheme,
+  systemSettingDark: systemSettingDark
+});
+class ThemeSwitcher {
+  constructor() {
+    this.initialize();
+  }
+  initialize() {
+    themeBtn.innerHTML = currentThemeSetting === "dark" ? lightHtml : darkHtml;
+    this.updateThemeOnHtml(currentThemeSetting);
+    themeBtn.addEventListener("click", this.handleToggleEvt.bind(this));
+  }
+  handleToggleEvt() {
+    const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
+    const aria = newTheme === "dark" ? "Change to light theme" : "Change to dark theme";
+    themeBtn.innerHTML = newTheme === "dark" ? lightHtml : darkHtml;
+    themeBtn.setAttribute("aria-label", aria);
+    this.updateThemeOnHtml(newTheme);
+    localStorage.setItem("theme", newTheme);
+    currentThemeSetting = newTheme;
+  }
+  updateThemeOnHtml(theme) {
+    document.querySelector("html").setAttribute("data-theme", theme);
+  }
 }
 
 /***/ }),
@@ -42,32 +112,38 @@ __webpack_require__.r(__webpack_exports__);
 
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_sourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default()));
-___CSS_LOADER_EXPORT___.push([module.id, "@import url(https://fonts.googleapis.com/css2?family=Nunito:wght@300&display=swap);"]);
-___CSS_LOADER_EXPORT___.push([module.id, "@import url(https://fonts.googleapis.com/css2?family=Nunito:wght@600&display=swap);"]);
-___CSS_LOADER_EXPORT___.push([module.id, "@import url(https://fonts.googleapis.com/css2?family=Nunito:wght@800&display=swap);"]);
+___CSS_LOADER_EXPORT___.push([module.id, "@import url(https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap);"]);
 // Module
 ___CSS_LOADER_EXPORT___.push([module.id, `:root {
     --white: hsl(0, 0%, 100%);
+    --trs-duration: 300ms;
+    --trs-timing: cubic-bezier(0.68, -0.55, 0.265, 1.55);
 }
 
 [data-theme="light"] {
-    --bg-color: hsl(0, 0%, 98%);
+    --bg-color: hsl(0, 0%, 94%);
     --ele-bg-color: var(--white);
     --txt-color: hsl(200, 15%, 8%);
     --ip-color: hsl(0, 0%, 52%);
+    --bxs-color: hsl(0, 0%, 58%);
 }
 
 [data-theme="dark"] {
     --bg-color: hsl(207, 26%, 17%);
-    --ele-bg-color:hsl(209, 23%, 22%);
+    --ele-bg-color: hsl(209, 23%, 22%);
     --txt-color: var(--white);
     --ip-color: var(--white);
+    --bxs-color: hsl(210, 23%, 30%);
 }
+
 html {
     box-sizing: border-box;
 
 }
-*, *::before, *::after{
+
+*,
+*::before,
+*::after {
     margin: unset;
     padding: unset;
     box-sizing: inherit;
@@ -76,7 +152,167 @@ html {
 body {
     background-color: var(--bg-color);
     color: var(--txt-color);
-}`, "",{"version":3,"sources":["webpack://./src/style.css"],"names":[],"mappings":"AAIA;IACI,yBAAyB;AAC7B;;AAEA;IACI,2BAA2B;IAC3B,4BAA4B;IAC5B,8BAA8B;IAC9B,2BAA2B;AAC/B;;AAEA;IACI,8BAA8B;IAC9B,iCAAiC;IACjC,yBAAyB;IACzB,wBAAwB;AAC5B;AACA;IACI,sBAAsB;;AAE1B;AACA;IACI,aAAa;IACb,cAAc;IACd,mBAAmB;AACvB;;AAEA;IACI,iCAAiC;IACjC,uBAAuB;AAC3B","sourcesContent":["@import url('https://fonts.googleapis.com/css2?family=Nunito:wght@300&display=swap');\n@import url('https://fonts.googleapis.com/css2?family=Nunito:wght@600&display=swap');\n@import url('https://fonts.googleapis.com/css2?family=Nunito:wght@800&display=swap');\n\n:root {\n    --white: hsl(0, 0%, 100%);\n}\n\n[data-theme=\"light\"] {\n    --bg-color: hsl(0, 0%, 98%);\n    --ele-bg-color: var(--white);\n    --txt-color: hsl(200, 15%, 8%);\n    --ip-color: hsl(0, 0%, 52%);\n}\n\n[data-theme=\"dark\"] {\n    --bg-color: hsl(207, 26%, 17%);\n    --ele-bg-color:hsl(209, 23%, 22%);\n    --txt-color: var(--white);\n    --ip-color: var(--white);\n}\nhtml {\n    box-sizing: border-box;\n\n}\n*, *::before, *::after{\n    margin: unset;\n    padding: unset;\n    box-sizing: inherit;\n}\n\nbody {\n    background-color: var(--bg-color);\n    color: var(--txt-color);\n}"],"sourceRoot":""}]);
+    font-family: "Nunito Sans", system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    transition: var(--trs-duration) background-color var(--trs-timing), color var(--trs-duration) var(--trs-timing);
+}
+
+button,
+input,
+input::placeholder,
+a {
+    font-family: inherit;
+
+}
+
+li {
+    list-style: none;
+}
+
+a {
+    color: inherit;
+    text-decoration: none;
+}
+
+button {
+    color: inherit;
+    cursor: pointer;
+    border: none;
+    background-color: inherit;
+}
+
+img {
+    max-width: 100%;
+}
+
+.container header>h1 {
+    font-weight: 800;
+    font-size: 1em;
+}
+
+.header,
+.main,
+.wrapper {
+    padding: 1.75em 1.15em;
+
+}
+
+.header {
+    display: grid;
+    background-color: var(--ele-bg-color);
+    grid-template-columns: 2fr 1fr;
+    place-content: center;
+}
+
+.header img {
+    width: 23px;
+
+}
+
+.header>button {
+    text-transform: capitalize;
+    display: flex;
+    align-items: center;
+    justify-content: end;
+    gap: 0.45em;
+    font-size: 0.85rem;
+    font-weight: 600;
+}
+
+.form>.search_container {
+    background-color: var(--ele-bg-color);
+    display: flex;
+    color: var(--ip-color);
+    align-items: center;
+    justify-content: center;
+    height: 50px;
+    padding-inline: 1.55em;
+    gap: 1.1em;
+    border-radius: .35em;
+    margin-bottom: 2em;
+}
+
+.form>.search_container svg {
+    color: cadetblue;
+    width: 30px;
+    flex: 0 1 30px;
+}
+
+.form>.search_container:has(> input:focus) {
+    outline: 2px solid cadetblue;
+    box-shadow: 0px 0px 10px cadetblue;
+    transition: all var(--trs-duration) var(--trs-timing);
+}
+
+.form>.search_container input {
+    background-color: inherit;
+    color: inherit;
+    border: none;
+    height: 60%;
+    font-size: 0.9rem;
+    width: 75%;
+    flex: 1 1 100%;
+
+}
+
+.form>.search_container input:focus {
+    outline: transparent;
+}
+
+.filter_wrapper button,
+.filter_wrapper button+div {
+    font-size: .88rem;
+    background-color: var(--ele-bg-color);
+    width: 14em;
+    border-radius: .35em;
+    padding: 1em .65em 1em 1.35em;
+}
+
+
+.filter_wrapper button {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 1em;
+    margin-bottom: 0.5em;
+}
+
+.filter_wrapper button svg {
+    width: 20px;
+    rotate: 0deg;
+    transition: rotate var(--trs-duration) var(--trs-timing);
+}
+
+.filter_wrapper button.expanded>svg {
+    rotate: 180deg;
+}
+
+.filter_wrapper button+div {
+    height: auto;
+    max-height: 0px;
+    overflow: hidden;
+    visibility: hidden;
+    opacity: 0;
+    transition: max-height var(--trs-duration) var(--trs-timing), visibility 0ms ease-in-out, opacity var(--trs-duration) var(--trs-timing);
+}
+
+.filter_wrapper button.expanded+div {
+    visibility: visible;
+    opacity: 1;
+    max-height: 190px;
+}
+
+.filter_wrapper button+div li {
+    margin-bottom: 0.65em;
+}
+
+.filter_wrapper button+div li:last-of-type {
+    margin-bottom: 0;
+}
+
+.filter_wrapper button+div li>a {
+    display: block;
+    width: 100%;
+}`, "",{"version":3,"sources":["webpack://./src/style.css"],"names":[],"mappings":"AAGA;IACI,yBAAyB;IACzB,qBAAqB;IACrB,oDAAoD;AACxD;;AAEA;IACI,2BAA2B;IAC3B,4BAA4B;IAC5B,8BAA8B;IAC9B,2BAA2B;IAC3B,4BAA4B;AAChC;;AAEA;IACI,8BAA8B;IAC9B,kCAAkC;IAClC,yBAAyB;IACzB,wBAAwB;IACxB,+BAA+B;AACnC;;AAEA;IACI,sBAAsB;;AAE1B;;AAEA;;;IAGI,aAAa;IACb,cAAc;IACd,mBAAmB;AACvB;;AAEA;IACI,iCAAiC;IACjC,uBAAuB;IACvB,kKAAkK;IAClK,+GAA+G;AACnH;;AAEA;;;;IAII,oBAAoB;;AAExB;;AAEA;IACI,gBAAgB;AACpB;;AAEA;IACI,cAAc;IACd,qBAAqB;AACzB;;AAEA;IACI,cAAc;IACd,eAAe;IACf,YAAY;IACZ,yBAAyB;AAC7B;;AAEA;IACI,eAAe;AACnB;;AAEA;IACI,gBAAgB;IAChB,cAAc;AAClB;;AAEA;;;IAGI,sBAAsB;;AAE1B;;AAEA;IACI,aAAa;IACb,qCAAqC;IACrC,8BAA8B;IAC9B,qBAAqB;AACzB;;AAEA;IACI,WAAW;;AAEf;;AAEA;IACI,0BAA0B;IAC1B,aAAa;IACb,mBAAmB;IACnB,oBAAoB;IACpB,WAAW;IACX,kBAAkB;IAClB,gBAAgB;AACpB;;AAEA;IACI,qCAAqC;IACrC,aAAa;IACb,sBAAsB;IACtB,mBAAmB;IACnB,uBAAuB;IACvB,YAAY;IACZ,sBAAsB;IACtB,UAAU;IACV,oBAAoB;IACpB,kBAAkB;AACtB;;AAEA;IACI,gBAAgB;IAChB,WAAW;IACX,cAAc;AAClB;;AAEA;IACI,4BAA4B;IAC5B,kCAAkC;IAClC,qDAAqD;AACzD;;AAEA;IACI,yBAAyB;IACzB,cAAc;IACd,YAAY;IACZ,WAAW;IACX,iBAAiB;IACjB,UAAU;IACV,cAAc;;AAElB;;AAEA;IACI,oBAAoB;AACxB;;AAEA;;IAEI,iBAAiB;IACjB,qCAAqC;IACrC,WAAW;IACX,oBAAoB;IACpB,6BAA6B;AACjC;;;AAGA;IACI,aAAa;IACb,8BAA8B;IAC9B,mBAAmB;IACnB,QAAQ;IACR,oBAAoB;AACxB;;AAEA;IACI,WAAW;IACX,YAAY;IACZ,wDAAwD;AAC5D;;AAEA;IACI,cAAc;AAClB;;AAEA;IACI,YAAY;IACZ,eAAe;IACf,gBAAgB;IAChB,kBAAkB;IAClB,UAAU;IACV,uIAAuI;AAC3I;;AAEA;IACI,mBAAmB;IACnB,UAAU;IACV,iBAAiB;AACrB;;AAEA;IACI,qBAAqB;AACzB;;AAEA;IACI,gBAAgB;AACpB;;AAEA;IACI,cAAc;IACd,WAAW;AACf","sourcesContent":["@import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap');\n\n\n:root {\n    --white: hsl(0, 0%, 100%);\n    --trs-duration: 300ms;\n    --trs-timing: cubic-bezier(0.68, -0.55, 0.265, 1.55);\n}\n\n[data-theme=\"light\"] {\n    --bg-color: hsl(0, 0%, 94%);\n    --ele-bg-color: var(--white);\n    --txt-color: hsl(200, 15%, 8%);\n    --ip-color: hsl(0, 0%, 52%);\n    --bxs-color: hsl(0, 0%, 58%);\n}\n\n[data-theme=\"dark\"] {\n    --bg-color: hsl(207, 26%, 17%);\n    --ele-bg-color: hsl(209, 23%, 22%);\n    --txt-color: var(--white);\n    --ip-color: var(--white);\n    --bxs-color: hsl(210, 23%, 30%);\n}\n\nhtml {\n    box-sizing: border-box;\n\n}\n\n*,\n*::before,\n*::after {\n    margin: unset;\n    padding: unset;\n    box-sizing: inherit;\n}\n\nbody {\n    background-color: var(--bg-color);\n    color: var(--txt-color);\n    font-family: \"Nunito Sans\", system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;\n    transition: var(--trs-duration) background-color var(--trs-timing), color var(--trs-duration) var(--trs-timing);\n}\n\nbutton,\ninput,\ninput::placeholder,\na {\n    font-family: inherit;\n\n}\n\nli {\n    list-style: none;\n}\n\na {\n    color: inherit;\n    text-decoration: none;\n}\n\nbutton {\n    color: inherit;\n    cursor: pointer;\n    border: none;\n    background-color: inherit;\n}\n\nimg {\n    max-width: 100%;\n}\n\n.container header>h1 {\n    font-weight: 800;\n    font-size: 1em;\n}\n\n.header,\n.main,\n.wrapper {\n    padding: 1.75em 1.15em;\n\n}\n\n.header {\n    display: grid;\n    background-color: var(--ele-bg-color);\n    grid-template-columns: 2fr 1fr;\n    place-content: center;\n}\n\n.header img {\n    width: 23px;\n\n}\n\n.header>button {\n    text-transform: capitalize;\n    display: flex;\n    align-items: center;\n    justify-content: end;\n    gap: 0.45em;\n    font-size: 0.85rem;\n    font-weight: 600;\n}\n\n.form>.search_container {\n    background-color: var(--ele-bg-color);\n    display: flex;\n    color: var(--ip-color);\n    align-items: center;\n    justify-content: center;\n    height: 50px;\n    padding-inline: 1.55em;\n    gap: 1.1em;\n    border-radius: .35em;\n    margin-bottom: 2em;\n}\n\n.form>.search_container svg {\n    color: cadetblue;\n    width: 30px;\n    flex: 0 1 30px;\n}\n\n.form>.search_container:has(> input:focus) {\n    outline: 2px solid cadetblue;\n    box-shadow: 0px 0px 10px cadetblue;\n    transition: all var(--trs-duration) var(--trs-timing);\n}\n\n.form>.search_container input {\n    background-color: inherit;\n    color: inherit;\n    border: none;\n    height: 60%;\n    font-size: 0.9rem;\n    width: 75%;\n    flex: 1 1 100%;\n\n}\n\n.form>.search_container input:focus {\n    outline: transparent;\n}\n\n.filter_wrapper button,\n.filter_wrapper button+div {\n    font-size: .88rem;\n    background-color: var(--ele-bg-color);\n    width: 14em;\n    border-radius: .35em;\n    padding: 1em .65em 1em 1.35em;\n}\n\n\n.filter_wrapper button {\n    display: flex;\n    justify-content: space-between;\n    align-items: center;\n    gap: 1em;\n    margin-bottom: 0.5em;\n}\n\n.filter_wrapper button svg {\n    width: 20px;\n    rotate: 0deg;\n    transition: rotate var(--trs-duration) var(--trs-timing);\n}\n\n.filter_wrapper button.expanded>svg {\n    rotate: 180deg;\n}\n\n.filter_wrapper button+div {\n    height: auto;\n    max-height: 0px;\n    overflow: hidden;\n    visibility: hidden;\n    opacity: 0;\n    transition: max-height var(--trs-duration) var(--trs-timing), visibility 0ms ease-in-out, opacity var(--trs-duration) var(--trs-timing);\n}\n\n.filter_wrapper button.expanded+div {\n    visibility: visible;\n    opacity: 1;\n    max-height: 190px;\n}\n\n.filter_wrapper button+div li {\n    margin-bottom: 0.65em;\n}\n\n.filter_wrapper button+div li:last-of-type {\n    margin-bottom: 0;\n}\n\n.filter_wrapper button+div li>a {\n    display: block;\n    width: 100%;\n}"],"sourceRoot":""}]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
