753481f4e652a847ff36149c6e5fd6098648fc9c
KMusyimi <kennmusyimi97@gmail.com>
2024-07-31 15:24:36 +0300

    add data asynchronously to the home page

--- src/homepage.js
+++ src/homepage.js
@@ -1,3 +1,46 @@
-export class HomePage{
-    
+const content = document.getElementById("content");
+let url = 'https://restcountries.com/v3.1/all?fields=name,population,region,capital,flags';
+export class HomePage
+{
+    constructor()
+    {
+        this.initialize();
+    }
+    initialize()
+    {
+        this.getData()
+    }
+    addCards(arr)
+    {
+        console.log('arr.length :>> ', arr.length);
+        arr.forEach(data =>
+        {
+            this.createCard(data);
+        });
+    }
+    createCard(data)
+    {
+        let html = `<article class="country_card">
+        <section>
+            <h1>${data.name['common']}</h1>
+            <p><span>population:</span> ${data.population}</p>
+            <p><span>region:</span> ${data.region}</p>
+            <p><span>capital:</span> ${data.capital}</p>
+        </section>
+        <figure><img src=${data.flags['svg']} alt="countries flags image" loading="lazy"></figure>
+        </article>`;
+        content.insertAdjacentHTML("beforeend", html);
+    }
+    async getData()
+    {
+
+        const dataPromise = await fetch(url);
+        const data = await dataPromise.json();
+        this.addCards(data);
+        // countries = data;
+
+        // console.log('data :>> ', data);
+        // console.log('countries.indexOf() :>> ', countries.indexOf(countries.name === "Kenya"));
+        // return data;
+    }
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
-import './style.css'
+import * as css from './style.css'
 import { ThemeSwitcher } from './theme';
+import { HomePage } from './homepage';
 
 let countries;
 window.addEventListener("load", () =>
 {
+    setTimeout(() =>
+        {
+            document.querySelector(".loading_container").classList.add("hidden");
+            document.querySelector(".container").classList.add('visible');
+        }, 2000);
+        const homepage = new HomePage();
+});
+document.addEventListener("DOMContentLoaded", () =>
+{
     const _ = new ThemeSwitcher();
     const dropdownBtn = document.querySelector(".dropdown_btn");
-    dropdownBtn.addEventListener("click", function(){
+    dropdownBtn.addEventListener("click", function ()
+    {
         dropdownBtn.classList.toggle("expanded");
     });
-    getData();
+    
+  getData()
 });
+
 
 async function getData()
 {
@@ -18,6 +31,8 @@ async function getData()
     const dataPromise = await fetch('https://restcountries.com/v3.1/all?fields=name,population,region,capital,flags');
     const data = await dataPromise.json();
     countries = data;
+
     console.log('data :>> ', data);
     console.log('countries.indexOf() :>> ', countries.indexOf(countries.name === "Kenya"));
+    return data;
 }
