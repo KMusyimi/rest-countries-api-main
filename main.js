b33322bb7ee826a5e8982a36c411217dec165ab7
KMusyimi <kennmusyimi97@gmail.com>
2024-08-12 21:11:58 +0300

    refactored the code and added a scroll.js

--- main.js
+++ main.js
@@ -24,10 +24,15 @@ class DetailsPage {
   initialize() {
     wrapper.style.display = 'none';
     this.detailsPageContent();
+    document.querySelector(".back-btn").addEventListener("click", this.backBtnEvt);
   }
   detailsPageContent() {
     content.innerHTML = '';
     this.createBackBtn();
+    if (this.data === undefined) {
+      content.insertAdjacentHTML("beforeend", '<p>Country does not exist</p>');
+      return;
+    }
     const lastKey = Object.keys(this.data.name['nativeName']).pop();
     const languages = Object.values(this.data.languages).join(', ');
     const html = `
@@ -36,14 +41,14 @@ class DetailsPage {
                     <header>
                         <h1 class='fw-800'>${this.data.name['common']}</h1>
                     </header>
-                    <div>
+                    <div class="details">
                         <p><span class="fw-600">native name:</span> ${this.data.name['nativeName'][lastKey]['common']}</p>
                         <p><span class="fw-600">population:</span> ${(0,_format_population__WEBPACK_IMPORTED_MODULE_0__.numberWithCommas)(this.data.population)}</p>
                         <p><span class="fw-600">region:</span> ${this.data.region}</p>
                         <p><span class="fw-600">sub region:</span> ${this.data.subregion}</p>
-                        <p><span class="fw-600">capital:</span> ${this.data.capital}</p>
+                        <p><span class="fw-600">capital:</span> ${this.data.capital.join(', ')}</p>
                     </div>
-                    <div>
+                    <div class="details">
                         <p><span class="fw-600">top level domain:</span> <span class='tld'>${this.data.tld[0]}</span></p>
                         <p><span class="fw-600">currencies:</span> ${this.currencyName(this.data.currencies)}</p>
                         <p><span class="fw-600">languages:</span> ${languages}</p>
@@ -52,7 +57,7 @@ class DetailsPage {
                 </section >
                 <figure class="flag"><img src="${this.data.flags['svg']}" alt="${this.data.flags['alt']}" loading="lazy"></figure>
             </article >
-    `;
+        `;
     content.insertAdjacentHTML("beforeend", html);
     this.populateBorderCountries();
   }
@@ -73,7 +78,7 @@ class DetailsPage {
     const borders = document.querySelector(".borders");
     const countriesWrapper = document.createElement("div");
     if (this.data.borders === undefined) {
-      countriesWrapper.innerHTML = '<span>No Border Countries</span>';
+      countriesWrapper.innerHTML = '<span class="bxs-bd">No Border Countries</span>';
       borders.insertAdjacentElement("beforeend", countriesWrapper);
       return;
     }
@@ -97,6 +102,9 @@ class DetailsPage {
         return el.name;
       }
     }
+  }
+  backBtnEvt() {
+    console.log('click :>> ');
   }
 }
 
@@ -130,8 +138,11 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var _details__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./details */ "./src/details.js");
 /* harmony import */ var _format_population__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./format_population */ "./src/format_population.js");
+/* harmony import */ var _scroll__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./scroll */ "./src/scroll.js");
 
 
+
+const scroll = new _scroll__WEBPACK_IMPORTED_MODULE_2__.Scroll();
 const content = document.getElementById("content");
 const searchInput = document.querySelector("#search");
 const searchForm = document.querySelector("#searchForm");
@@ -142,13 +153,6 @@ class HomePage {
     this.initialize();
   }
   initialize() {
-    this.backToTopElement({
-      bottom: "3em",
-      right: "2em"
-    });
-    window.addEventListener("scroll", () => {
-      this.displayButtonOnScroll();
-    });
     searchForm.addEventListener("submit", this.formSubmit.bind(this));
     this.getData(url);
     this.searchInput();
@@ -179,33 +183,6 @@ class HomePage {
     article.className = "country_card";
     article.innerHTML = html;
     content.insertAdjacentElement("beforeend", article);
-  }
-  backToTopElement(_ref) {
-    let {
-      bottom,
-      right
-    } = _ref;
-    const button = document.createElement("button");
-    button.type = "button";
-    button.id = "back_top";
-    button.innerHTML = `<svg viewBox="0 0 24 24" fill="currentColor"><path class="cls-1" d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm4.707,9.707a1,1,0,0,1-1.414,0L13,8.414V18a1,1,0,0,1-2,0V8.414L8.707,10.707A1,1,0,1,1,7.293,9.293l4-4a1,1,0,0,1,1.414,0l4,4A1,1,0,0,1,16.707,10.707Z"/></svg>`;
-    button.style.bottom = bottom;
-    button.style.right = right;
-    button.style.display = 'none';
-    button.addEventListener("click", this.scrollTopEvt, false);
-    content.insertAdjacentElement("beforeend", button);
-  }
-  displayButtonOnScroll() {
-    const btn = document.getElementById("back_top");
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      btn.style.display = 'block';
-    } else {
-      btn.style.display = 'none';
-    }
-  }
-  scrollTopEvt() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
   }
   async getData(url) {
     const dataPromise = await fetch(url);
@@ -241,7 +218,6 @@ class HomePage {
     const url = `https://restcountries.com/v3.1/name/${country}?fullText=true`;
     const data = await this.individualItem(url);
     const detailsPage = new _details__WEBPACK_IMPORTED_MODULE_0__.DetailsPage(data[0]);
-    console.log('url :>> ', url);
   }
   // TODO: refactor code 
   filterByRegion() {
@@ -251,8 +227,10 @@ class HomePage {
   }
   filterEvt(evt) {
     evt.preventDefault();
+    searchForm.reset();
+    search.focus();
     content.innerHTML = '';
-    this.backToTopElement({
+    scroll.backToTopButton({
       bottom: "3em",
       right: "2em"
     });
@@ -274,6 +252,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ "./src/style.css");
 /* harmony import */ var _theme__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./theme */ "./src/theme.js");
 /* harmony import */ var _homepage__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./homepage */ "./src/homepage.js");
+/* harmony import */ var _scroll__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./scroll */ "./src/scroll.js");
+
 
 
 
@@ -282,6 +262,7 @@ document.addEventListener("DOMContentLoaded", () => {
   document.getElementById('searchForm').reset();
   const dropdownBtn = document.querySelector(".dropdown_btn");
   const homepage = new _homepage__WEBPACK_IMPORTED_MODULE_2__.HomePage();
+  const scroll = new _scroll__WEBPACK_IMPORTED_MODULE_3__.Scroll();
   setTimeout(() => {
     document.querySelector(".loading_container").classList.add("hidden");
     document.querySelector(".container").classList.add('visible');
@@ -289,11 +270,6 @@ document.addEventListener("DOMContentLoaded", () => {
       document.querySelector(".loading_container").style.display = 'none';
     }, 100);
   }, 2000);
-  window.scrollBy({
-    top: 20,
-    left: 0,
-    behavior: 'smooth'
-  });
   dropdownBtn.addEventListener("click", function () {
     dropdownBtn.classList.toggle("expanded");
     setTimeout(() => {
@@ -301,6 +277,67 @@ document.addEventListener("DOMContentLoaded", () => {
     }, 20000);
   });
 });
+
+/***/ }),
+
+/***/ "./src/scroll.js":
+/*!***********************!*\
+  !*** ./src/scroll.js ***!
+  \***********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   Scroll: () => (/* binding */ Scroll)
+/* harmony export */ });
+class Scroll {
+  constructor() {
+    this.initialize();
+  }
+  initialize() {
+    this.backToTopButton({
+      bottom: "3em",
+      right: "2em"
+    });
+    window.scrollBy({
+      top: 20,
+      left: 0,
+      behavior: 'smooth'
+    });
+    window.addEventListener("scroll", () => {
+      this.displayButtonOnScroll();
+    });
+  }
+  displayButtonOnScroll() {
+    const btn = document.getElementById("back_top");
+    if (btn != null) {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        btn.style.display = 'block';
+      } else {
+        btn.style.display = 'none';
+      }
+    }
+  }
+  backToTopButton(_ref) {
+    let {
+      bottom,
+      right
+    } = _ref;
+    const button = document.createElement("button");
+    button.type = "button";
+    button.id = "back_top";
+    button.innerHTML = `<svg viewBox="0 0 24 24" fill="currentColor"><path class="cls-1" d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm4.707,9.707a1,1,0,0,1-1.414,0L13,8.414V18a1,1,0,0,1-2,0V8.414L8.707,10.707A1,1,0,1,1,7.293,9.293l4-4a1,1,0,0,1,1.414,0l4,4A1,1,0,0,1,16.707,10.707Z"/></svg>`;
+    button.style.bottom = bottom;
+    button.style.right = right;
+    button.style.display = 'none';
+    button.addEventListener("click", this.backToTopEvt, false);
+    content.insertAdjacentElement("beforeend", button);
+  }
+  backToTopEvt() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+}
 
 /***/ }),
 
