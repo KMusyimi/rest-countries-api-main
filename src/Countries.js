8458559e0ee1046cc98da358ee87d95b49f28758
KMusyimi <kennmusyimi97@gmail.com>
2024-11-05 13:45:29 +0300

    fixed bugs

--- src/Countries.js
+++ src/Countries.js
@@ -40,15 +40,15 @@ export class Countries extends AbstractView
         {
             const nameLowerCase = data.name['common'].replace(/\s+/g, '-').toLowerCase();
             return `
-            <a href="/page/${nameLowerCase}" data-link><article id='${nameLowerCase}' class='country_card'>
+            <article id='${nameLowerCase}' class='country_card'>
                 <section>
-                    <h1 class="fw-800">${data.name['common']}</h1>
+                    <h1 class="fw-800"><a id='country__link' href="/page/${nameLowerCase}" data-link>${data.name['common']}</a></h1>
                     <p><span class="fw-600">population:</span> ${this.numberWithCommas(data.population)}</p>
                     <p><span class="fw-600">region:</span> ${data.region}</p>
                     <p><span class="fw-600">capital:</span> ${this.formatCapital(data)}</p>
                 </section>
                 <figure><img src=${data.flags['png']} alt="${data.flags['alt']}" loading="lazy"></figure>
-            </article></a>`;
+            </article>`;
         }).join('');
     }
 }
