61c0d7bc31017c2c7452f3bb828a5179f07aec72
KMusyimi <kennmusyimi97@gmail.com>
2024-11-05 13:58:13 +0300

    fixed bugs

--- src/Countries.js
+++ src/Countries.js
@@ -14,7 +14,7 @@ export class Countries extends AbstractView
     async getHtml()
     {
         const region = location.pathname.substring(1) || '';
-
+        
 
         document.getElementById("content").innerHTML = '';
         document.getElementById("content").className = 'main';
@@ -41,17 +41,14 @@ export class Countries extends AbstractView
             const nameLowerCase = data.name['common'].replace(/\s+/g, '-').toLowerCase();
             return `
             <article id='${nameLowerCase}' class='country_card'>
-            <a href="/page/${nameLowerCase}" data-link>
                 <section>
-                    <h1 class="fw-800">${data.name['common']}</h1>
+                    <h1 class="fw-800"><a id='country__link' href="/page/${nameLowerCase}" data-link>${data.name['common']}</a></h1>
                     <p><span class="fw-600">population:</span> ${this.numberWithCommas(data.population)}</p>
                     <p><span class="fw-600">region:</span> ${data.region}</p>
                     <p><span class="fw-600">capital:</span> ${this.formatCapital(data)}</p>
                 </section>
                 <figure><img src=${data.flags['png']} alt="${data.flags['alt']}" loading="lazy"></figure>
-                </a>
-            </article>`
-                ;
+            </article>`;
         }).join('');
     }
 }
